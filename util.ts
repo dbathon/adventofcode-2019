165f5322d4db1a423561b8ded003100a9e798bb7
Dominik Bathon <dominik.bathon@gmail.com>
2019-12-15 16:45:42 +0100

    Implement Map2D
    Map2D allows easily working with two dimensionsal data with arbitrary
    coordinates.

--- util.ts
+++ util.ts
@@ -10,7 +10,7 @@ export function sum(numbers: number[]) {
 }
 
 export function p(input: any) {
-  console.log(JSON.stringify(input));
+  console.log(typeof input === "string" ? input : JSON.stringify(input));
 }
 
 export function splitArray<T>(array: T[], splitLength: number): T[][] {
@@ -32,4 +32,86 @@ export function findMax<T>(list: T[], toNumber: (t: T) => number): { max: number
     }
   });
   return { max, maxElement };
+}
+
+/**
+ * A 2D map of T elements around (0,0). It grows as needed in all directions (but is not sparse).
+ */
+export class Map2D<T> {
+  private data: T[] = [];
+  dimension = 10;
+  originX = 0;
+  originY = 0;
+
+  copy(): Map2D<T> {
+    const result: Map2D<T> = new Map2D();
+    result.data = this.data.slice(0);
+    result.dimension = this.dimension;
+    result.originX = this.originX;
+    result.originY = this.originY;
+    return result;
+  }
+
+  private getIndex(x: number, y: number): number {
+    const xIndex = x - this.originX;
+    const yIndex = y - this.originY;
+    if (xIndex < 0 || xIndex >= this.dimension || yIndex < 0 || yIndex >= this.dimension) {
+      // we need to grow
+      const oldMap: Map2D<T> = this.copy();
+
+      this.data.length = 0;
+      if (xIndex < 0) {
+        this.originX = x;
+      }
+      if (yIndex < 0) {
+        this.originY = y;
+      }
+      if (xIndex >= this.dimension || yIndex >= this.dimension) {
+        this.dimension = Math.max(xIndex, yIndex) + 1;
+      }
+
+      // now just copy the data from oldMap
+      for (let y = 0; y < oldMap.dimension; ++y) {
+        for (let x = 0; x < oldMap.dimension; ++x) {
+          const realX = oldMap.originX + x;
+          const realY = oldMap.originY + y;
+          this.set(realX, realY, oldMap.get(realX, realY));
+        }
+      }
+      return this.getIndex(x, y);
+    }
+    return (yIndex * this.dimension) + xIndex;
+  }
+
+  get(x: number, y: number): T {
+    const index = this.getIndex(x, y);
+    return index === undefined ? undefined : this.data[index];
+  }
+
+  set(x: number, y: number, value: T): void {
+    if (value !== undefined) {
+      this.data[this.getIndex(x, y)] = value;
+    }
+  }
+
+  getAsArray(): T[][] {
+    const result: T[][] = [];
+    for (let y = 0; y < this.dimension; ++y) {
+      const row: T[] = [];
+      result.push(row);
+      for (let x = 0; x < this.dimension; ++x) {
+        row[x] = this.get(this.originX + x, this.originY + y);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * This only works if all the elements are single character strings.
+   */
+  draw(): string {
+    return this.getAsArray().map(row => row.map(element => {
+      return element === undefined ? " " : element;
+    }).join("")).join("\n");
+  }
 }
